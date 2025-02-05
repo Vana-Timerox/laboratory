513ec0dc26ced54fb7bd8708bd570e6fd5c01d29
Justine <justinesarmiento906@gmail.com>
2025-02-05 18:46:25 +0800

    ginagawa ko login

--- login.php
+++ login.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <title>Login  page</title>
 </head>
 <body>
-    <p>This is LOGIN</p>
+    <div class="container">
+        <h1 class="form-title">Sign In</h1>
+        <form action="" method="POST">
+            <div class="input-group">
+                <i class="fas fa-envelop"></i>
+                <input type="email" name="email" id="email" required placeholder="Email">
+            </div>
+        </form>
+    </div>
 </body>
 </html>
